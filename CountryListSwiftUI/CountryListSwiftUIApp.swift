07022f1b0afc26b152163da270c30a66692bfd0a
Dhathri33 <dbathini@luc.edu>
2025-10-15 09:28:50 -0500

    Initial Commit

---
+++ CountryListSwiftUI/CountryListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryListSwiftUIApp.swift
+//  CountryListSwiftUI
+//
+//  Created by Dhathri Bathini on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CountryListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
